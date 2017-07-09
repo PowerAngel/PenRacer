65607229d82f412cf135a76d2db7966f22ada161
Jonatan <Jonatan.Berg@live.com>
2017-07-09 13:10:24 +0200

    Added BoardManager script

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour {
+	public static BoardManager instance;
+	public List<Sprite> characters = new List<Sprite>();
+	public GameObject tile;
+	public int xSize, ySize;
+
+	private GameObject[,] tiles;
+
+	public bool IsShifting{ get; set; }
+	// Use this for initialization
+	void Start () {
+		instance = GetComponent<BoardManager> ();
+		Vector2 offset = tile.GetComponent<SpriteRenderer> ().bounds.size;
+		CreateBoard (offset.x, offset.y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void CreateBoard(float xOffset, float yOffset)
+	{
+		tiles = new GameObject[xSize, ySize];
+
+		float startX = transform.position.x;
+		float startY = transform.position.y;
+
+		for (int x = 0; x < xSize; x++) {
+			for (int y = 0; y < ySize; y++) {
+				GameObject newTile = Instantiate (tile, new Vector3 (startX + (xOffset * x), startY + (yOffset * y), 0), tile.transform.rotation);
+				tiles [x, y] = newTile;
+			}
+		}
+	}
+}
